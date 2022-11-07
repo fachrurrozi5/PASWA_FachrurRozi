4277a3dd65a47e0d4af63958281bc63871fe53fc
fachrurrozi5 <rozifachrur511@gmail.com>
2022-11-07 21:21:12 +0700

    oji

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,29 +39,62 @@
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
-                            <form class="user" method="post" action="pro_login.php">
-                                <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>PASWA 2022</h3>
-                            
-                            <h3>Sign In</h3>
+                        <?php
+                                    include("connection.php");
+
+                                    if (isset($_POST['submit'])) {
+                                        $user = mysqli_real_escape_string($mysqli, $_POST['username']);
+                                        $pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+                                        
+
+                                        if ($user == "" || $pass == "") {
+                                            echo "<script language=javascript>
+                                    alert('Username atau Password Kosong');
+                                    window.location='login.php';
+                                </script>";
+                                        } else {
+                                            $result = mysqli_query($mysqli, "SELECT * FROM tab_user 
+                                WHERE username='$user' AND password=md5('$pass')")
+                                                or die("Instruksi tidak ditemukan");
+                                            $row = mysqli_fetch_assoc($result);
+
+                                            if (is_array($row) && !empty($row)) {
+                                                $validuser = $row['username'];
+                                                $_SESSION['valid'] = $validuser;
+                                                $_SESSION['id'] = $row['id'];
+                                            } else {
+                                                echo "<script language=javascript>
+                                            alert('Username atau Password salah');
+                                            window.location='login.php';
+                                        </script>";
+                                            }
+
+                                            if (isset($_SESSION['valid'])) {
+                                                header('Location: dashboard.php');
+                                            }
+                                        }
+                                    } else {
+                                    ?>
+
+                                        <form method="POST" action="">
+                                <a href="index.php" class="">
+                                <h3 class="text-primary"><img class="rounded-circle" src="asset/img/paswa.png" alt="" style="width: 40px; height: 40px;"></i>PASWA 2022</h3>
+                                </a>
+                                <h3>Login Admin</h3>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="text" name="username" class="form-control form-control-user" id="floatingInput" placeholder="name">
+                            <input type="text" name="username" class="form-control form-control-user" placeholder="Enter Username">
                             <label for="floatingInput">Username </label>
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
                             <label for="floatingPassword">Password</label>
                         </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <a href="">Forgot Password</a>
-                        </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
-                        </form>
+                        
+                        <input type="submit" name="submit" value="submit" class="btn btn-primary btn-user btn-block">
+                                            <hr>
+                                        </form>
+                                    <?php } ?>
                     </div>
                 </div>
             </div>
@@ -80,7 +114,7 @@
     <script src="asset/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="asset/js/main.js"></script>
 </body>
 
 </html>
